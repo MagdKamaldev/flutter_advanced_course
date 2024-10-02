c2db22559dcaa5e8a9d204e6cf2fc865bb3e2769
magd <magddevf@gmail.com>
2024-10-02 13:41:03 +0300

    -added spacing widgets -added more colors and text styles -added font weight helper -added general text button and text form field -finished login screen design

--- lib/core/routing/app_router.dart
+++ lib/core/routing/app_router.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_advanced_course/core/routing/routes.dart';
 import 'package:flutter_advanced_course/features/login/ui/login_screen.dart';
 import 'package:flutter_advanced_course/features/on_boarding/ui/on_boarding_screen.dart';
+import 'package:flutter_advanced_course/features/signup/ui/signup_screen.dart';
 
 class AppRouter{
   Route generateRoute(RouteSettings settings){
@@ -11,6 +12,8 @@ class AppRouter{
         return MaterialPageRoute(builder: (_) => const OnBoardingScreen());
       case Routes.loginScreen:
         return MaterialPageRoute(builder: (_) => const LoginScreen());
+        case Routes.signUpScreen:
+        return MaterialPageRoute(builder: (_) => const SignUpScreen());
       default:
         return MaterialPageRoute(builder: (_) => Scaffold(body: Center(child: Text("no route defined for ${settings.name}")),));
     }
--- lib/features/login/ui/login_screen.dart
+++ lib/features/login/ui/login_screen.dart
@@ -1,12 +1,84 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_advanced_course/core/helpers/spacing.dart';
+import 'package:flutter_advanced_course/core/theming/styles.dart';
+import 'package:flutter_advanced_course/core/widgets/app_text_button.dart';
+import 'package:flutter_advanced_course/core/widgets/app_text_form_field.dart';
+import 'package:flutter_advanced_course/features/login/ui/widgets/donot_have_account_text.dart';
+import 'package:flutter_advanced_course/features/login/ui/widgets/terms_and_conditions_text.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
 
-class LoginScreen extends StatelessWidget {
+class LoginScreen extends StatefulWidget {
   const LoginScreen({super.key});
 
   @override
+  State<LoginScreen> createState() => _LoginScreenState();
+}
+
+class _LoginScreenState extends State<LoginScreen> {
+  final formKey = GlobalKey<FormState>();
+  bool isObscureText = false;
+  @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: const Text("LoginScreen")),
-      body: Center(child: Text("LoginScreen")));
+      body: SafeArea(
+        child: SingleChildScrollView(
+          child: Padding(
+            padding: EdgeInsets.symmetric(horizontal: 30.w, vertical: 30.h),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text(
+                  "Welcome Back",
+                  style: TextStyles.font24BlueBold,
+                ),
+                verticalSpace(8),
+                Text(
+                  "We're excited to have you back, can't wait to see what you've been up to since you last logged in.",
+                  style: TextStyles.font14GreyRegular,
+                ),
+                verticalSpace(36),
+                Form(
+                    key: formKey,
+                    child: Column(
+                      children: [
+                        const AppTextFormField(hintText: "Email"),
+                        verticalSpace(20),
+                        AppTextFormField(
+                          hintText: "Password",
+                          isObscureText: isObscureText,
+                          suffixIcon: GestureDetector(
+                              onTap: () {
+                                setState(() {
+                                  isObscureText = !isObscureText;
+                                });
+                              },
+                              child: Icon(isObscureText
+                                  ? Icons.visibility_off
+                                  : Icons.visibility)),
+                        ),
+                        verticalSpace(24),
+                        Align(
+                            alignment: AlignmentDirectional.centerEnd,
+                            child: Text(
+                              "Forgot Password?",
+                              style: TextStyles.font13BlueRegular,
+                            )),
+                        verticalSpace(40),
+                        AppTextButton(
+                            buttonText: "Login",
+                            textStyle: TextStyles.font16WhiteMedium,
+                            onPressed: () {}),
+                            verticalSpace(16),
+                            const TermsAndConditionsText(),
+                            verticalSpace(16),
+                            const DontHaveAccountText()
+                      ],
+                    ))
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
   }
 }
